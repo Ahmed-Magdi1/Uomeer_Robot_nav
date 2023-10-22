ec530e1c48f7ed5cfc33b6db04f77024618a8ebd
Ahmed Magdi <128742811+Ahmed-Magdi1@users.noreply.github.com>
2023-10-22 20:52:39 +0300

    update list

--- build/uomeer_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmed/uomeer_ws/src/uomeer_description
-# Build directory: /home/ahmed/uomeer_ws/build/uomeer_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
